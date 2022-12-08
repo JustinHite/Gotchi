0e74d8f00df68b1df2eb4ffd8362df23ce0a10a2
JustinHite <hitej@ccf.org>
2022-12-07 22:49:36 -0500

    Created main methods for Gotchi

--- VirtPet.py
+++ VirtPet.py
@@ -13,6 +13,54 @@ class VirtPet:
         self.hunger = 0
         self.size = 1
         self.life = 100
+        
+    def play(self, happiness):
+        self.happiness = self.happiness + 5
+        return self.happiness
+    
+    def eat(self, hunger):
+        self.hunger = self.hunger + 5
+        return self.hunger
+    
+    def _sadness(self, happiness):
+        self.happiness = self.happiness - 1
+        return self.happiness
+        
+    def _hunger(self, hunger):
+        self.hunger = self.hunger - 1
+        return self.hunger
+        
+    def _health(self, life, happiness, hunger):
+        if self.happiness < 10:
+            self.life = self.life - 1
+        elif self.happiness > 20:
+            self.life = self.life + 1
+        
+        if self.hunger < 10:
+            self.life = self.life - 1
+        elif self.hunger > 20:
+            self.life = self.life + 1
+        
+    def _age(self, age):
+        self.age = self.age + 1
+        return self.age
+    
+    def _deathCheck(self, age, life):
+        death = False
+        if self.age > 25:
+            death = True
+        elif self.life < 1:
+            death = True
+        return death
+
+            
+            
+    
+    
+            
+        
+        
+        
     
         
     
